a47330e6c47a699696da9f18ff0bd3627c5757d2
zhorta <horta.z@gmail.com>
2018-12-13 19:01:42 +0100

    Correction of a mistake
    The JSON was not correct. I duplicated the same thing over and over again.

--- gtfsToJSON.py
+++ gtfsToJSON.py
@@ -1,3 +1,6 @@
+# Code that converts GTFS files from the RATP_GTFS_FULL and RATP_GTFS_LINES folders to JSON
+# so we can put them in a database
+
 import json
 import os
 
@@ -26,12 +29,15 @@ def pywalkerFull(path):
                         data = {}
                         data2 = {}
                         for line in inputFile:
-                                currentLine = line.split(",")
-                                currentLine[len(currentLine)-1] = currentLine[len(currentLine)-1][:-1]
+                                currentLine = line.split(",")             
+                                currentLine[len(currentLine)-1] = currentLine[len(currentLine)-1][:-1] #to remove the \n
+                                data[k] = {}
                                 for i in range(len(titles)):
-                                        data2[titles[i]] = currentLine[i]
-                                        data[currentLine[0]] = data2
-                        inputFile.close()      
+                                      data[k][titles[i]] = currentLine[i]
+                                k = k + 1
+                                
+                                      
+                        inputFile.close()
 
                         writeToJSONFile(directoryWrite, fileName, data)
 
@@ -43,26 +49,32 @@ def pywalkerLines(path):
         folder = root.split("/")[-1]
         directoryWrite = "./" +"JSON Files Lines" + '/' + folder + "_JSON"
         directoryRead = "./" + folder
+        print(folder)
         if not os.path.exists(directoryWrite):
                 os.makedirs(directoryWrite)
         
         for file_ in files:
+                k = 0
                 fileName = file_.split(".")[0] 
                 if(fileName != ""):
                         directoryRead = "./RATP_GTFS_LINES/" + folder + "/" + fileName + ".txt"
+                        
                         inputFile = open(directoryRead, 'r')
                         firstLine = inputFile.readline()[:-1] #to remove the \n
                         titles = firstLine.split(",")
                         data = {}
                         data2 = {}
                         for line in inputFile:
-                                currentLine = line.split(",")
+                                currentLine = line.split(",")             
                                 currentLine[len(currentLine)-1] = currentLine[len(currentLine)-1][:-1] #to remove the \n
+                                data[k] = {}
                                 for i in range(len(titles)):
-                                      data2[titles[i]] = currentLine[i]
-                                      data[currentLine[0]] = data2
-                        inputFile.close()      
-
+                                      data[k][titles[i]] = currentLine[i]
+                                k = k + 1
+                                
+                                      
+                        inputFile.close()
+                              
                         writeToJSONFile(directoryWrite, fileName, data)
 
 pywalkerLines('./RATP_GTFS_LINES')
